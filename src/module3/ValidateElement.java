7da43ff9827250223a7fd15d6776fdfa67a819e2
tabhresh <tabhresh@gmail.com>
2019-02-04 10:46:34 +0530

    3rd Feb 2019

--- src/module3/ValidateElement.java
+++ src/module3/ValidateElement.java
@@ -1,37 +1,31 @@
 package module3;
 
-
-
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ImplicitWait {
+public class ValidateElement {
 
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
-		driver.get("http://newtours.demoaut.com/");
-		
-		//Implicitly wait
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		//PageLoad
+
+		driver.get("http://seleniumautomationpractice.blogspot.com/2017/10/functionisogramigoogleanalyticsobjectri.html");
+
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		
-	
-		driver.findElement(By.name("userName")).sendKeys("tutorial");
-		driver.findElement(By.name("password")).sendKeys("tutorial");
-		driver.findElement(By.name("login")).click();
+		driver.findElement(By.id("profession-0")).click();
+		Thread.sleep(5000);
 		
-		driver.findElement(By.linkText("PROFILE")).click();
+		WebElement element = driver.findElement(By.id("profession-0"));
 		
-		driver.close();
+		boolean result = element.isSelected();
 		
+		System.out.println(result);
 	}
 
 }
--- src/module3/Xpath.java
+++ src/module3/Xpath.java
@@ -7,28 +7,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Xpath {
 
 	public static void main(String[] args) {
-	
-		//Open Browser
+
+		// Instantiate the Browser
 		System.setProperty("webdriver.chrome.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+
+		// Open the AUT
+		driver.get("https://seleniumautomationpractice.blogspot.com/2017/10/functionisogramigoogleanalyticsobjectri.html");
 		
-		//Open AUT
-		driver.get("https://www.facebook.com/");		
+		driver.findElement(By.xpath("//*[@name='lastname']//preceding::input[1]")).sendKeys("Abhresh");
 		
-		//Enter details in the form using Customized Xpath
-		driver.findElement(By.xpath("//input[@id='u_0_j']")).sendKeys("Abhresh");
+		driver.findElement(By.xpath("//*[@name='lastname']//following::input[9]")).click();
 		
+		driver.findElement(By.xpath("//*[contains(@name,'profession')]//following::input[5]")).click();
 		
-		//Select the date using Following and Preceding
-		driver.findElement(By.xpath("//*[@name='email']//following::input[1]")).sendKeys("Sugandhi");
-		driver.findElement(By.xpath("//*[@name='pass']//preceding::input[1]")).sendKeys("Abhresh");
-		
-		
-		//Select Year
-		driver.findElement(By.xpath("//select[@id='month']//following::select/option[3]")).click();
-		
-		//Select Date
-		driver.findElement(By.xpath("//select[@id='month']//preceding::select/option[4]")).click();
+		driver.findElement(By.xpath("//*[contains(@id, 'continents')]//following-sibling::option[4]")).click();
+	
 		
 	}
 
